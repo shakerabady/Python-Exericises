598ea593aa6517f81e799db9359161aa21847f35
SHAKER <shakerdrais@gmail.com>
2019-11-19 16:33:46 +0200

    excs are done except the ones that cant be coded

---
+++ temp.py
@@ -0,0 +1,65 @@
+print ('{:5} {:10} {:15}' .format("hello\n","hello\n","hello\n"))
+
+
+print("orange\n"*20)
+
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep='10')
+
+x=1
+y=2.8
+z=1j
+o="orange"
+A=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+
+x,y="Orange",1
+X1,Y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(Y1)
+
+x,y="Orange",1
+X1,y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(Y1)
+
+
+#print("hello, World")
+print("cheers,mate!") #this is the program
+"""testing
+the code"""
+
+age=int(input("what is your age"))
+till100=(100-age)
+age100=(till100+2020)
+print(age100)
+
+
+base=int(input("enter the base of the triangle"))
+height=int(input("enter the height of the triangle"))
+
+triangleArea= 0.5*base*height
+print(triangleArea)
+
+# [ ]create variables to store input: name, age, get_mail with prompts
+name,age,get_mail=input("enter your name: "), input("enter your age: "), input("do you want to get mail: ")
+# for name, age and yes/no to being on an email list
+
+
+# [ ] print a description + variable value for each variable
+print("enter your first name:", name)
+print("enter your age: ", age)
+print("do you want to get mail: ", get_mail)
+
+
